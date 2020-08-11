6a724ec10e4d48e293565f0b650a57a94665dd8b
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-08-12 00:53:46 +0900

    Update mobile.tsx

--- pages/mobile/mobile.tsx
+++ pages/mobile/mobile.tsx
@@ -5,10 +5,11 @@ const Mobile = () => {
   const [viewRect, setViewRect] = React.useState([0, 0]);
   React.useEffect(() => {
     setViewRect([window.innerWidth, window.innerHeight]);
+    window.addEventListener("resize", () =>
+      setViewRect([window.innerWidth, window.innerHeight])
+    );
   }, []);
-  window.addEventListener("resize", () =>
-    setViewRect([window.innerWidth, window.innerHeight])
-  );
+
   return (
     <Wrapper style={{ width: `${viewRect[0]}px`, height: `${viewRect[1]}px` }}>
       <WorkPage author={"fumin"} />
