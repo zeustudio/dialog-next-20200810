e4888690decf518f733a5d63d617663b3e8b0545
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-08-11 23:45:38 +0900

    Update overview.tsx

--- components/mobile/overview.tsx
+++ components/mobile/overview.tsx
@@ -31,15 +31,28 @@ const Overview: React.FC<Props> = ({ author }) => {
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (min-height: 813px) {
+    justify-content: space-evenly;
+  }
 `;
 const Thumb = styled.img`
   width: 100%;
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
 `;
 const CaptionWrapper = styled.div`
   margin: 5%;
   font-size: 2rem;
   word-wrap: break-word;
   line-height: 2.5rem;
+  overflow: scroll;
+  @media screen and (min-width: 600px) {
+    width: 600px;
+  }
 `;
 const TitleJP = styled.div``;
 const TitleEN = styled.div``;
