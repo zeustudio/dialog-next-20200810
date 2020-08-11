7b871465820688054939e079df2b9df56a64c9f6
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-08-12 01:01:41 +0900

    Update mobile.tsx

--- pages/mobile/mobile.tsx
+++ pages/mobile/mobile.tsx
@@ -2,16 +2,8 @@ import React from "react";
 import styled from "@emotion/styled";
 import WorkPage from "../../components/mobile/workpage";
 const Mobile = () => {
-  const [viewRect, setViewRect] = React.useState([0, 0]);
-  React.useEffect(() => {
-    setViewRect([window.innerWidth, window.innerHeight]);
-    window.addEventListener("resize", () =>
-      setViewRect([window.innerWidth, window.innerHeight])
-    );
-  }, []);
-
   return (
-    <Wrapper style={{ width: `${viewRect[0]}px`, height: `${viewRect[1]}px` }}>
+    <Wrapper>
       <WorkPage author={"fumin"} />
     </Wrapper>
   );
